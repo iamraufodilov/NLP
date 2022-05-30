90711ec7537383e6f73310172fe609503a75cc2f
raufodilov <applyjobrauf@gmail.com>
2022-05-30 20:08:32 +0200

    Update

---
+++ Sentiment Analysis/preprocessing.py
@@ -0,0 +1,24 @@
+# load necessary libraries
+
+
+# data path
+path = "C:/Users/user/AppData/Roaming/nltk_data/corpora/twitter_samples/"
+
+# create some functions to preprocess data
+def remove_noice(tweet_tokens, stop_words = ()):
+    cleaned_tokens = []
+    for token, tag in post_tag(tweet_tokens):
+        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
+                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+','', token)
+        token = re.sub("(@[A-Za-z0-9_]+)","", token)
+
+        if tag.startswith('NN'):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else:
+            pos = 'a'
+
+        lemmatizer = WordNetLemmatizer()
+        token = lemmatizer.lemmatize(token, pos)
+
